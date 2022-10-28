a3fa2db590863af4d438cf3c159701d98deb39e7
Christopher Burkhead <chrisburkhead@gmail.com>
2022-10-27 22:32:08 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CS311_Project2_CMB
+{
+    partial class frmCraps
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDice1 = new System.Windows.Forms.Label();
+            this.lblDice2 = new System.Windows.Forms.Label();
+            this.lblBank = new System.Windows.Forms.Label();
+            this.txtBet = new System.Windows.Forms.TextBox();
+            this.lblBet1 = new System.Windows.Forms.Label();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.lblWinLosePoint = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDice1
+            // 
+            this.lblDice1.AutoSize = true;
+            this.lblDice1.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDice1.Location = new System.Drawing.Point(201, 138);
+            this.lblDice1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDice1.Name = "lblDice1";
+            this.lblDice1.Size = new System.Drawing.Size(0, 85);
+            this.lblDice1.TabIndex = 0;
+            // 
+            // lblDice2
+            // 
+            this.lblDice2.AutoSize = true;
+            this.lblDice2.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDice2.Location = new System.Drawing.Point(366, 138);
+            this.lblDice2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDice2.Name = "lblDice2";
+            this.lblDice2.Size = new System.Drawing.Size(0, 85);
+            this.lblDice2.TabIndex = 1;
+            // 
+            // lblBank
+            // 
+            this.lblBank.AutoSize = true;
+            this.lblBank.Location = new System.Drawing.Point(920, 105);
+            this.lblBank.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblBank.Name = "lblBank";
+            this.lblBank.Size = new System.Drawing.Size(0, 29);
+            this.lblBank.TabIndex = 2;
+            // 
+            // txtBet
+            // 
+            this.txtBet.Location = new System.Drawing.Point(769, 178);
+            this.txtBet.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtBet.Name = "txtBet";
+            this.txtBet.PlaceholderText = "Enter Your Bet";
+            this.txtBet.Size = new System.Drawing.Size(267, 36);
+            this.txtBet.TabIndex = 3;
+            this.txtBet.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBet1
+            // 
+            this.lblBet1.AutoSize = true;
+            this.lblBet1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblBet1.Location = new System.Drawing.Point(769, 105);
+            this.lblBet1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblBet1.Name = "lblBet1";
+            this.lblBet1.Size = new System.Drawing.Size(82, 29);
+            this.lblBet1.TabIndex = 4;
+            this.lblBet1.Text = "Bank";
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRoll.Location = new System.Drawing.Point(769, 270);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(267, 187);
+            this.btnRoll.TabIndex = 5;
+            this.btnRoll.Text = "ROLL!";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // lblWinLosePoint
+            // 
+            this.lblWinLosePoint.AutoSize = true;
+            this.lblWinLosePoint.Font = new System.Drawing.Font("Stencil", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblWinLosePoint.Location = new System.Drawing.Point(201, 339);
+            this.lblWinLosePoint.Name = "lblWinLosePoint";
+            this.lblWinLosePoint.Size = new System.Drawing.Size(0, 52);
+            this.lblWinLosePoint.TabIndex = 6;
+            this.lblWinLosePoint.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frmCraps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 522);
+            this.Controls.Add(this.lblWinLosePoint);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.lblBet1);
+            this.Controls.Add(this.txtBet);
+            this.Controls.Add(this.lblBank);
+            this.Controls.Add(this.lblDice2);
+            this.Controls.Add(this.lblDice1);
+            this.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frmCraps";
+            this.Text = "CRAPS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblDice1;
+        private Label lblDice2;
+        private Label lblBank;
+        private TextBox txtBet;
+        private Label lblBet1;
+        private Button btnRoll;
+        private Label lblWinLosePoint;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+/*
+ * This in an application that simulates the Casino game of craps
+ * Author Chris Burkhead
+ * CS311-ON
+ * Fall 2022
+ * Bellarmine University
+ * Version 1.0
+ */
+
+namespace CS311_Project2_CMB
+{
+    public partial class frmCraps : Form
+    {
+        //instance variables
+        int point = 0;
+        double bank = 100.00;
+        double bet;
+
+        //constructor
+        public frmCraps()
+        {
+            InitializeComponent();
+
+        }//end constructor
+
+        // Form1_Load() sets a standard format for the lblBank and sets it equal to the variable bank
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBank.Text = "$" + bank.ToString("0.00");
+            Console.WriteLine(lblBank);
+
+        }//end Form1_Load
+
+
+        /* btnRoll_Click() checks to ensure that a bet has been placed and then ensures that 
+         * enough funds exist in the bank to cover the bet. If both conditions are met then 
+         * the calculate_score() is called. Otherwise MessageBoxes pop up informaing the player what changes 
+         * they must make in order to keep playing. 
+        */
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+           /* If structure checks to ensure that txtBet isn't empty, if it is it throws a message box
+            * informing the player to place a bet and then exits the method. Otherwise if a bet has been placed
+            * it sets the variable bet equal to the amount entered by the player.
+            */
+            if (txtBet.Text == "")
+            {
+                MessageBox.Show("You must enter a Bet to play");
+                return;
+
+            }//end if
+
+            else if (txtBet.Text !="")
+            {
+                bet = float.Parse(txtBet.Text);
+            }//end else if
+            
+            
+            /* If structure checks three conditions, 1st if checks to see if the bank is greater or equal 
+             *  to the bet placed, if true it calls the calculate_score() and game play proceeds. 
+             *  2nd else if checks to see if the bank is empty, if this is true then play halts.
+             *  3rd else if checks to see if the bet made is greater then whats in the bank, if this is true
+             *  it directs the player to make a smaller bet
+             */
+            if (bank >= bet)
+            { 
+                calculate_score();
+            }//end if
+
+            else if (bank == 0)
+            {
+                MessageBox.Show("Sorry, your bank is empty, please play again");
+            }// end else if
+
+            else if (bank < bet)
+            {
+                MessageBox.Show("You do not have enough in the bank to make this bet, please bet again");
+
+            }//end else if
+           
+        }//end btnRoll_Click
+
+        /* calculate_score() runs the main simulation of the game using if and switch structures.
+         * It generates two random numbers between 1-6 that represent dice. It then finds the sum of 
+         * those dice and uses two switch structures to evaulate the sum of those dice against the conditions
+         * of the game.
+         */
+        private void calculate_score()
+        {
+            //resetting all lbl's to blank before each roll
+            lblDice1.Text = "";
+            lblDice2.Text = "";
+            lblWinLosePoint.Text = "";
+            
+           //genertating random numbers between 1-6 storing those values into variables and then finding their sum
+            Random d1 = new Random();
+            Random d2 = new Random();
+            int dice1 = d1.Next(1, 7);
+            int dice2 = d2.Next(1, 7);
+            int diceSum = dice1 + dice2;
+
+            //writing the value of the dice to the screen
+            lblDice1.Text = dice1.ToString();
+            lblDice2.Text = dice2.ToString();
+            Console.WriteLine(lblDice1);
+            Console.WriteLine(lblDice2);
+
+
+            /* This if structure has three conditions which execute the rules of the game. 
+             * The 1st if statement checks to see if the variable point is Zero, if this is true 
+             * that means this is the first roll of the game which specific conditions apply and 
+             * are evaluated by a switch statement. The switch statement evaluates if the case is a winning, 
+             * losing or default case and then prints the result to the screen. It also adjusts the value of the 
+             * bank based on if the roll was a winning or losing roll. This game uses a standard 1:1 return on bets
+             * in a craps game. It also resets the point variable to 0 if it was a winning or losing case. If it is
+             * a default case then the variable point gets set to the value of the variable diceSum as it determines 
+             * the execution of the next two else if statements. 
+             */
+            if (point == 0)
+            {
+                switch (diceSum)
+                {
+                    case 7:
+                        lblWinLosePoint.Text = ("You Win!");
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        bank = bank + bet;
+                        lblBank.Text = "$" + bank.ToString("0.00"); 
+                        Console.Write(lblBank);
+                        point = 0;
+                        break;
+
+                    case 11:
+                        lblWinLosePoint.Text = ("You Win!");
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        bank = bank + bet;
+                        lblBank.Text = "$" + bank.ToString("0.00");
+                        Console.Write(lblBank);
+                        point = 0;
+                        break;
+
+                    case 2:
+                        lblWinLosePoint.Text = ("Craps! House Wins");
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        bank = bank - bet;
+                        lblBank.Text = "$" + bank.ToString("0.00");
+                        Console.Write(lblBank);
+                        point = 0;
+                        break;
+
+                    case 3:
+                        lblWinLosePoint.Text = ("Craps! House Wins");
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        bank = bank - bet;
+                        lblBank.Text = "$" + bank.ToString("0.00");
+                        Console.Write(lblBank);
+                        point = 0;
+                        break;
+
+                    case 12:
+                        lblWinLosePoint.Text = ("Craps! House Wins");
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        bank = bank - bet;
+                        lblBank.Text = "$" + bank.ToString("0.00");
+                        Console.Write(lblBank);
+                        point = 0;
+                        break;
+
+                    default:
+                        point = diceSum;
+                        lblWinLosePoint.Text = "Point is " + point;
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        break;
+                }//end switch
+
+            }//end if
+
+            /* This else if statement is checking to see if a winning condition has been met 
+             * after the inital roll. It does this by ensuring that point does not equal 0 and it is also equal
+             * to the new diceSum. This ensures that it is atleast the second roll of a game where a winning or 
+             * losing condition was not met in the previous switch structure. And diceSum is the value of 
+             * the new roll. If point and diceSum are equal then the player wins. It then prints the 
+             * results to the screen. And resets point to 0 so a new game can start.
+             */
+            else if ((point != 0) && (point == diceSum))
+            {
+                lblWinLosePoint.Text = ("You Win!");
+                Console.WriteLine(lblWinLosePoint.Text);
+                bank = bank + bet;
+                lblBank.Text = "$" + bank.ToString("0.00");
+                Console.Write(lblBank);
+                point = 0;
+            }// end else if
+
+            /*The last else if statement only executes if this is at least the second roll of the game by ensuring
+             * variable point is not equal to zero. It then uses a switch structure consisting of one case and
+             * one default statement. The case is checking to see if the new roll is equal to 7, because after the
+             * first roll 7 now becomes a losing condition and it executes exactly how the other losing conditions 
+             * operate. The default case executes if the losing condition was not meet and reprints the
+             * current point to the screen, which prompts the player to roll again.  
+             */
+            else if (point != 0)
+            { 
+                switch (diceSum)
+                {
+                    case 7:
+                        lblWinLosePoint.Text = ("Craps! House Wins");
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        bank = bank - bet;
+                        lblBank.Text = "$" + bank.ToString("0.00");
+                        Console.Write(lblBank);
+                        point = 0;
+                        break;
+
+                    default:
+                        lblWinLosePoint.Text = "Point is " + point;
+                        Console.WriteLine(lblWinLosePoint.Text);
+                        break;
+                }//end switch
+
+            }// end else if
+           
+        }//end calculate_score
+  
+    }//end class
+
+}//end namespace
